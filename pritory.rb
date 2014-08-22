2dea40650e842f8e03477ce5e8471b582fd30c70
Panagiotis Atmatzidis <atma@convalesco.org>
2014-08-22 18:17:30 +0300

    First try to access global var '@squick'

--- pritory.rb
+++ pritory.rb
@@ -1,15 +1,17 @@
 # encoding: utf-8
 require 'sinatra'
 require 'haml'
-require 'oauth2'
-require 'json'
 require 'sinatra/session'
 require 'sinatra/partial'
 require 'sinatra/flash'
 require 'bluecloth'
 require 'haml'
-require 'logger'
+# require 'logger'
 require 'openssl'
+require 'oauth2'
+require 'net/http'
+require 'json'
+require 'faraday'
 
 require_relative 'minify_resources'
 require_relative 'mysecrets'
@@ -21,7 +23,7 @@ class Pritory < Sinatra::Base
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
   # Log what happens
-  $log = Logger.new('log/output.log')
+  # $log = Logger.new('log/output.log')
 
   configure do
     register Sinatra::Session
@@ -66,6 +68,79 @@ class Pritory < Sinatra::Base
     include Rack::Utils
     alias_method :h, :escape_html
 
+    # Skroutz Module (needs to be loaded by routes)
+    module Skroutz
+      class Query
+        # OAuth2 client object
+        def client
+          client ||= OAuth2::Client.new(MySecrets::SKROUTZ_OAUTH_CID, MySecrets::SKROUTZ_OAUTH_PAS, site: 'https://skroutz.gr', authorize_url: "/oauth2/authorizations/new", token_url: "/oauth2/token", user_agent: 'pritory')
+        end
+
+        # Request token
+        def get_token
+          begin
+            # Remember to define the scope here (e.g. 'public')
+            t = client.client_credentials.get_token(scope: 'public') 
+          rescue OAuth2::Error => e
+            e.response.status == 404 ? "Not Found: 404" : e
+          end
+        end
+
+        # Skroutz search product category (step 1)
+        def query_skroutz keyword
+          begin
+            raise ArgumentError.new("Keyword is too small!") if keyword.length < 3
+            token =  get_token.token
+            con = Faraday.new
+            con.params = {oauth_token: token}
+            con.headers = {user_agent: 'pritory'}
+            con.headers = {'Accept' => 'application/vnd.skroutz+json; version=3'}
+            r1 = con.get "http://api.skroutz.gr/api/search?q=#{keyword}"
+            JSON.parse(r1.body)
+          rescue ArgumentError => e
+            puts e
+          rescue OAuth2::Error => e
+            puts "\nResponse headers: #{e.response.headers}"
+          end
+        end
+
+        # Search for categories (step 2)
+        def query_skroutz2 id, name
+          begin
+            token =  get_token.token
+            con = Faraday.new
+            con.params = {oauth_token: token}
+            con.headers = {user_agent: 'pritory'}
+            con.headers = {'Accept' => 'application/vnd.skroutz+json; version=3'}
+            r1 = con.get "http://api.skroutz.gr/api/categories/#{id}/skus?q=#{name}"
+            JSON.parse(r1.body)
+          rescue ArgumentError => e
+            puts e
+          rescue OAuth2::Error => e
+            puts "\nResponse headers: #{e.response.headers}"
+          end
+        end
+
+        def query_skroutz3 id
+          begin
+            token =  get_token.token
+            con = Faraday.new
+            con.params = {oauth_token: token}
+            con.headers = {user_agent: 'pritory'}
+            con.headers = {'Accept' => 'application/vnd.skroutz+json; version=3'}
+            r1 = con.get "http://api.skroutz.gr/api/skus/#{id}/products"
+            JSON.parse(r1.body)
+          rescue ArgumentError => e
+            puts e
+          rescue OAuth2::Error => e
+            puts "\nResponse headers: #{e.response.headers}"
+          end
+        end
+      end
+    end
+
+
+    # Access Control
     # Login required
     def protected! 
       # halt [ 401, 'Not Authorized' ] unless session? 
@@ -85,6 +160,9 @@ class Pritory < Sinatra::Base
       haml :not_found
     end
   end
+
+  # Define 'squick', accessible by routes
+  @squick ||= Skroutz::Query.new
 end
 
 require_relative 'models/init'
